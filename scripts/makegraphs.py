769b4d9884ceae2d16bd0dd54ba5357309abb206
Mudit Gupta <mudit@berkeley.edu>
2019-05-07 19:21:00 -0700

    Smoothen graphs, fix padding, move legend, fix iteration order of machines

--- scripts/makegraphs.py
+++ scripts/makegraphs.py
@@ -11,6 +11,24 @@ import warnings
 from common import SERVERS, machine_name, mkdir_safe
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.interpolate import interp1d
+
+def smoothen(x, y):
+
+	x=np.array(x)
+	y=np.array(y)
+
+	x_new = np.linspace(x.min(), x.max(),500)
+
+	f = interp1d(x, y, kind='quadratic')
+	y_smooth = f(x_new)
+
+	return x_new, y_smooth
+
+	# plt.plot (x_new,y_smooth)
+	# plt.scatter (x, y)
+
+
 
 def load_dir(dirname):
 	input_dirname = "parsed_outputs/" + dirname
@@ -18,12 +36,12 @@ def load_dir(dirname):
 
 	directory_path = "graphs/" + dirname
 	# If the directory exists, ask user whether it should be deleted.
-	if (os.path.isdir(directory_path)):
-		print("WARNING: Directory exists and will be deleted. Continue? [y/Y]")
-		if input() in ['y', 'Y']:
-			shutil.rmtree(directory_path)
-		else:
-			exit()
+	# if (os.path.isdir(directory_path)):
+	# 	print("WARNING: Directory exists and will be deleted. Continue? [y/Y]")
+	# 	if input() in ['y', 'Y']:
+	# 		shutil.rmtree(directory_path)
+	# 	else:
+	# 		exit()
 
 	data = {}
 
@@ -32,7 +50,7 @@ def load_dir(dirname):
 	return data
 	
 
-def make_all_graphs(all_data, dirname, title, indices=[1, 2, 4]):
+def make_all_graphs(all_data, dirname, title, savedir, indices=[1, 2, 4]):
 	experiments = {
 		1: 'Recieved Data MiB',
 		2: 'Trasmitted Data MiB',
@@ -48,50 +66,36 @@ def make_all_graphs(all_data, dirname, title, indices=[1, 2, 4]):
 	number_of_items = len(a_machine_values[0]) - 1
 
 
-	plt.figure(figsize=(8.3,11.7))
-	plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=None, hspace=0.4)
+	plt.figure(figsize=(8.3, 11.7))
+	plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0, hspace=0.4)
 
+	# Done to fix the order of the keys...
+	alldata_keys = list(all_data.keys())
 
 	counter = 1
 	for i in indices:
-		data = {}
+		data = []
 
-		for k, v in all_data.items():
+		for k in alldata_keys:
+			v = all_data[k]
 			# Collect the i-th column from v.
-			data[k] = [float(val[i]) for val in v]
+			data.append((k, [float(val[i]) for val in v]))
 
 		y_label = experiments[i]
 		plt.subplot(len(indices), 1, counter)
 		make_graph(data, times, y_label)
 
 		if counter == 1:
-			plt.title(title, loc='center', pad=40)
+			plt.title(title, loc='center', pad=20)
+			plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.4),
+          ncol=6, fancybox=True, shadow=True)
 
 		counter += 1
 
 
-	
-	mkdir_safe('graphs/' + dirname)
-
-# x = np.arange(10)
-
-# fig = plt.figure()
-# ax = plt.subplot(111)
-
-# for i in xrange(5):
-#     ax.plot(x, i * x, label='$y = %ix$'%i)
-
-# # Shrink current axis by 20%
-# box = ax.get_position()
-# ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
-
-# # Put a legend to the right of the current axis
-# ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-
-# plt.show()
-
-	
-	plt.savefig('graphs/' + dirname + '/graph.png')
+	print("savedir is ", savedir)
+	mkdir_safe('graphs/' + savedir)
+	plt.savefig('graphs/' + savedir + '/graph.png')
 
 	
 
@@ -110,12 +114,12 @@ def parseraw_file(filepath):
 
 def make_graph(data, labels, y_label):
 	"""
-	Data is a dictionary of the form
-	 {
-		'machine_name': [rx1, rx2, rx3, ...],
-		'machine_name2': [rx1, rx2, rx3, ...],
+	Data is a list of the form
+	 [
+		('machine_name': [rx1, rx2, rx3, ...]),
+		('machine_name2': [rx1, rx2, rx3, ...]),
 		...
-	 }
+	 ]
 
 	Labels is a list having the same length as 
 	the lists that are the values of data. 
@@ -125,12 +129,18 @@ def make_graph(data, labels, y_label):
 	"""
 	print(data)
 
-	for name, vals in data.items():
-		plt.plot(labels, vals, label=name)
+	for x in data:
+		name, vals = x
+
+		labels = [i*5 for i in range(len(labels))]
+		smooth_labels, smooth_vals = smoothen(labels, vals)
+		smooth_vals = smooth_vals.clip(min=0)
+
+		plt.plot(smooth_labels, smooth_vals, label=name)
 
 	plt.ylabel(y_label)
-	plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.2),
-          ncol=3, fancybox=True, shadow=True)
+	plt.xlabel("minutes")
+	
 
 
 
@@ -141,10 +151,13 @@ if __name__ == "__main__":
 
 	parser.add_argument("dirname", help="directory to be created in ./graphs/ with the graph")
 	parser.add_argument("title", help="directory to be created in ./outputs/ with the csvs")
+	parser.add_argument("savedir", help="place result here", nargs='?', default=None)
 	args = parser.parse_args()
 
+	print(args.savedir)
+
 	all_data = load_dir(args.dirname)
-	make_all_graphs(all_data, args.dirname, args.title)
+	make_all_graphs(all_data, args.dirname, args.title, args.savedir if args.savedir else args.dirname)
 
 	
 	
